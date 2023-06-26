e1b07698a905385778f23a2e1496d7069f36e785
Wellington Batista <wellingtonbs109@gmail.com>
2023-06-26 18:34:18 -0300

    feach: created products component and init back

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,8 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+//components
+import { ProdutosComponent } from '../components/produtos/produtos.component';
 
 @NgModule({
   imports: [
@@ -14,6 +16,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    ProdutosComponent
+  ]
 })
 export class HomePageModule {}
