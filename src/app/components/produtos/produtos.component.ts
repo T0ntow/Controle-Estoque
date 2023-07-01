1770ec81f4849518cb30e80277f6db6b015ff559
Wellington Batista <wellingtonbs109@gmail.com>
2023-07-01 14:28:48 -0300

    feach: added filter by date

--- src/app/components/produtos/produtos.component.ts
+++ src/app/components/produtos/produtos.component.ts
@@ -3,6 +3,7 @@ import { ProductsService } from 'services/products.service';
 import { format } from 'date-fns';
 import { PopoverController } from '@ionic/angular';
 import { EditProductComponent } from '../popovers/edit-product/edit-product.component';
+import { utcToZonedTime } from 'date-fns-tz';
 
 @Component({
   selector: 'app-produtos',
@@ -10,14 +11,20 @@ import { EditProductComponent } from '../popovers/edit-product/edit-product.comp
   styleUrls: ['./produtos.component.scss'],
 })
 
+
+
 export class ProdutosComponent implements OnInit {
   searchTerm: string = "";
   products: any[] = [];
   marcas: string[] = [];
   filteredProducts: any[] = [];
-  selectedMark: string = '';
+
+  selectedFilter: string | null = null; // Valor inicial é null
 
   sortOrder: string = "asc"; // Valor padrão é "asc" para ordem crescente
+  selectedDate!: string;
+  selectedMark: string = '';
+
 
   constructor(
     private productService: ProductsService,
@@ -32,23 +39,40 @@ export class ProdutosComponent implements OnInit {
     this.getProducts()
   }
 
-  filterProductsByMarca(event: any) {
-    this.selectedMark = event.target.value;
-
-    if (this.selectedMark) {
-      this.filteredProducts = this.products.filter(product =>
+  filterProducts(filterType: string) {
+    this.selectedFilter = filterType;
+  
+    const timeZone = 'America/Sao_Paulo'; // Fuso horário desejado
+  
+    let filteredProducts = this.products;
+  
+    if (this.selectedFilter === 'date' && this.selectedDate) {
+      const zonedDate = utcToZonedTime(this.selectedDate, timeZone);
+      // Formatar a data selecionada no formato 'DD/MM/YYYY'
+      const formattedDate = format(zonedDate, 'dd/MM/yyyy');
+  
+      // Filtrar por data
+      filteredProducts = filteredProducts.filter(product =>
+        product.Data === formattedDate
+      );
+    }
+  
+    else if (this.selectedFilter === 'mark' && this.selectedMark) {
+      // Filtrar por marca
+      filteredProducts = filteredProducts.filter(product =>
         product.Marca.toLowerCase() === this.selectedMark.toLowerCase()
       );
-
-      this.popoverController.dismiss(); //fechar popover apos filtrar
-
-    } else {
-      this.filteredProducts = this.products;
     }
+  
+    this.filteredProducts = filteredProducts;
+  
+    console.log(this.filteredProducts);
   }
-
+  
   removeFilters() {
+    this.selectedFilter = '';
     this.selectedMark = '';
+    this.selectedDate = '';
 
     this.getProducts();
     this.popoverController.dismiss(); //fechar popover
@@ -68,7 +92,7 @@ export class ProdutosComponent implements OnInit {
       next: (data: any) => {
         this.products = data as any[];
         console.log(this.products);
-        
+
         this.formatDates(); // Chame a função formatDates após obter os produtos
         this.filteredProducts = this.products;
         this.getMarks(); // Chame a função getMarks após obter os produtos
@@ -81,12 +105,19 @@ export class ProdutosComponent implements OnInit {
   }
 
   sortProducts() {
+    //copia dos produtos filrados
+    let sortedProducts = [...this.filteredProducts];
+  
     if (this.sortOrder === "asc") {
-      this.products.sort((a, b) => a.Codigo - b.Codigo);
+      sortedProducts.sort((a, b) => a.Codigo - b.Codigo);
     } else if (this.sortOrder === "desc") {
-      this.products.sort((a, b) => b.Codigo - a.Codigo);
+      sortedProducts.sort((a, b) => b.Codigo - a.Codigo);
     }
+  
+    this.filteredProducts = sortedProducts;
   }
+  
+
   formatDates() {
     this.products.forEach(product => {
       product.Data = format(new Date(product.Data), 'dd/MM/yyyy');
@@ -96,7 +127,7 @@ export class ProdutosComponent implements OnInit {
   getMarks() {
     this.marcas = Array.from(new Set(this.products.map(product => product.Marca)));
   }
-  
+
   async openPopoverEdit(event: any, product: any) {
     const popover = await this.popoverController.create({
       component: EditProductComponent,
@@ -105,11 +136,11 @@ export class ProdutosComponent implements OnInit {
       componentProps: {
         product: product
       },
-      
+
     });
 
     await popover.present();
   }
- 
-  
+
+
 }
