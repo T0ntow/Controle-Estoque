26c5337eb0e936e4cd3f4caf3420f009329e272e
Wellington Batista <wellingtonbs109@gmail.com>
2023-10-15 20:59:46 -0300

    feat: updated sigup functions

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SignupSqlService } from 'services/signup/signup-sql.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
@@ -11,9 +14,10 @@ export class SignupPage implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private signupSqlService: SignupSqlService
-  ) 
-  {
+    private signupSqlService: SignupSqlService,
+    private router: Router,
+    private alertController: AlertController
+  ) {
     this.signupForm = this.formBuilder.group({
       nome: ['', Validators.required],
       email: ['', [Validators.required]],
@@ -26,19 +30,46 @@ export class SignupPage implements OnInit {
 
   newUser() {
     console.log("new user");
-    
+
     if (this.signupForm.valid) {
       const userData = this.signupForm.value;
 
       this.signupSqlService.newUser(userData).subscribe({
-        next: (response) => {
+        next: async (response) => {
           console.log('Usuario cadastrado com sucesso:', response);
+          await this.presentSignupSuccessAlert();
+          this.router.navigate(['/login']);
         },
-        error: (error) => {
+        error: async (error) => {
           console.error('Erro ao cadastrar usuario:', error);
+
+          if (error.error.error === "E-mail já em uso") {
+            await this.presentEmailInUseAlert();
+          }
+
         }
       })
     }
   }
 
+  async presentSignupSuccessAlert() {
+    const alert = await this.alertController.create({
+      header: 'Cadastro bem-sucedido',
+      message: 'Sua conta foi cadastrada com sucesso!',
+      buttons: ['OK']
+    });
+  
+    await alert.present();
+  }
+  
+  async presentEmailInUseAlert() {
+    const alert = await this.alertController.create({
+      header: 'Erro',
+      subHeader: 'E-mail já em uso',
+      message: 'O e-mail inserido já está associado a uma conta. Tente usar um e-mail diferente.',
+      buttons: ['OK']
+    });
+  
+    await alert.present();
+  }
 }
