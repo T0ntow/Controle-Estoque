ed6ee33789c231395d08a535f172ce84c42d89bc
Wellington Batista <wellingtonbs109@gmail.com>
2023-10-19 22:16:43 -0300

    feat: added function downloadPdf

--- src/app/pages/report/report.page.ts
+++ src/app/pages/report/report.page.ts
@@ -1,27 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import jsPDF from 'jspdf'; // Import the jspdf library
+import html2canvas from 'html2canvas'
+// import html2pdf from 'html2pdf.js';
+import * as html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.page.html',
   styleUrls: ['./report.page.scss'],
 })
+
 export class ReportPage implements OnInit {
   registros: any[] = [];
   registrosFiltrados: any[] = []; // Nova lista para registros filtrados
+  @ViewChild('content', { static: false }) content!: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
-    // Exemplo de registros (você pode obter os dados de uma API ou banco de dados)
     this.registros = [
       { tipo: 'Entrada', nomeProduto: 'Produto A', quantidade: 10, dataHora: '2023-10-18 10:00:00' },
       { tipo: 'Saída', nomeProduto: 'Produto B', quantidade: 5, dataHora: '2023-10-18 14:30:00' },
       { tipo: 'Entrada', nomeProduto: 'Produto C', quantidade: 8, dataHora: '2023-10-18 15:45:00' },
-      // Adicione mais registros conforme necessário
-    ];
-    this.registrosFiltrados = this.registros;
+      { tipo: 'Entrada', nomeProduto: 'Produto D', quantidade: 10, dataHora: '2023-10-18 10:00:00' },
+      { tipo: 'Saída', nomeProduto: 'Produto E', quantidade: 5, dataHora: '2023-10-18 14:30:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto F', quantidade: 8, dataHora: '2023-10-18 15:45:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto G', quantidade: 10, dataHora: '2023-10-18 10:00:00' },
+      { tipo: 'Saída', nomeProduto: 'Produto H', quantidade: 5, dataHora: '2023-10-18 14:30:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto I', quantidade: 8, dataHora: '2023-10-18 15:45:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto J', quantidade: 10, dataHora: '2023-10-18 10:00:00' },
+      { tipo: 'Saída', nomeProduto: 'Produto K', quantidade: 5, dataHora: '2023-10-18 14:30:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto L', quantidade: 8, dataHora: '2023-10-18 15:45:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto M', quantidade: 10, dataHora: '2023-10-18 10:00:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto A', quantidade: 10, dataHora: '2023-10-18 10:00:00' },
+      { tipo: 'Saída', nomeProduto: 'Produto B', quantidade: 5, dataHora: '2023-10-18 14:30:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto C', quantidade: 8, dataHora: '2023-10-18 15:45:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto D', quantidade: 10, dataHora: '2023-10-18 10:00:00' },
+      { tipo: 'Saída', nomeProduto: 'Produto E', quantidade: 5, dataHora: '2023-10-18 14:30:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto F', quantidade: 8, dataHora: '2023-10-18 15:45:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto G', quantidade: 10, dataHora: '2023-10-18 10:00:00' },
+      { tipo: 'Saída', nomeProduto: 'Produto H', quantidade: 5, dataHora: '2023-10-18 14:30:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto I', quantidade: 8, dataHora: '2023-10-18 15:45:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto J', quantidade: 10, dataHora: '2023-10-18 10:00:00' },
+      { tipo: 'Saída', nomeProduto: 'Produto K', quantidade: 5, dataHora: '2023-10-18 14:30:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto L', quantidade: 8, dataHora: '2023-10-18 15:45:00' },
+      { tipo: 'Entrada', nomeProduto: 'Produto M', quantidade: 10, dataHora: '2023-10-18 10:00:00' },
 
+    ];
+
+    this.registrosFiltrados = this.registros;
   }
+
   filtrarRegistros(event: any) {
     const searchTerm = event.target.value;
     if (searchTerm) {
@@ -35,14 +64,25 @@ export class ReportPage implements OnInit {
     }
   }
 
-  imprimirRelatorio() {
-    // Lógica para imprimir o relatório
-    // Você pode usar uma biblioteca ou serviço de impressão aqui
+  downloadPdf() {
+    const content = document.getElementById("content");
+
+    const doc = new jsPDF({
+      orientation: "portrait",
+      format: [800, 1400]
+    });
+
+    doc.html(content!, {
+      callback: function (pdf) {
+
+        pdf.save("relatorio.pdf");
+      }
+    });
   }
 
   mostrarFiltro() {
     // Lógica para mostrar opções de filtro (por data, semana, mês)
     // Você pode abrir um modal ou outra interface para configurar o filtro
   }
-  
+
 }
